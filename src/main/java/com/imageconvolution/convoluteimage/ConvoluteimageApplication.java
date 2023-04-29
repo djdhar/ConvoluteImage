20a530e3a507c9c89a1ce1ad1b3558dd7e5acb96
Dibyajyoti Dhar <dibyajyotidhar@gmail.com>
2023-04-29 23:23:34 +0530

    first commit

---
+++ src/main/java/com/imageconvolution/convoluteimage/ConvoluteimageApplication.java
@@ -0,0 +1,13 @@
+package com.imageconvolution.convoluteimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvoluteimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvoluteimageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imageconvolution/convoluteimage/ConvoluteimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imageconvolution.convoluteimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvoluteimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
